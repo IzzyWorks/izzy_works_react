cfd17a1273e3fafb507562ff1af010dc61d27ba8
Izzy <izzy.aspler@gmail.com>
2021-08-05 10:29:56 -0700

    package.json config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  //Where files should be sent once they are bundled
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  //webpack 5 comes with devServer which loads in development mode
+  devServer: {
+    port: 5000,
+    writeToDisk: true,
+    contentBase: './dist',
+  },
+  //Rules of how webpack will take our files, complie & bundle them for the browser
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/templates/index.html' }),
+    new MiniCssExtractPlugin(),
+  ],
+};

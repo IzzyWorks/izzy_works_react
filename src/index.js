b949d1c769d7fb1e4460259ce69a828c44842c4e
Izzy <izzy.aspler@gmail.com>
2021-09-12 15:53:59 -0700

    temporaraly using react-bootstrap components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+} from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+//components
+import NavBar from '../src/components/nav/NavBar';
+import LandingPage from '../src/components/whitepaperComps/WhitePaper';
+import AboutPage from '../src/components/homePage/About';
+import './components/scss/main.scss';
+
+//data
+import ContentData from './components/data/ContentData';
+
+const routes = [
+  { path: '/', name: 'Home', Component: LandingPage },
+  {
+    path: ContentData.path,
+    name: ContentData.id,
+    Component: ContentData,
+  },
+  // { path: '/tldnr', name: 'tldnr', Component: Home },
+  // { path: '/hapmap', name: 'HapMap', Component: About },
+  { path: '/about', name: 'About', Component: AboutPage },
+];
+
+function Example() {
+  return (
+    <Router>
+      <>
+        <NavBar bg='light'>
+          <NavBar className='mx-auto'>
+            {routes.map((route) => (
+              <Nav.Link
+                key={route.path}
+                as={NavLink}
+                to={route.path}
+                activeClassName='active'
+                exact
+              >
+                {route.name}
+              </Nav.Link>
+            ))}
+          </NavBar>
+        </NavBar>
+        <Container className='index-container'>
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames='page'
+                  unmountOnExit
+                >
+                  <div className='page'>
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </Container>
+      </>
+    </Router>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Example />, rootElement);
